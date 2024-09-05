3d134ea09690d8b24c7eb19610c03018d0e010fe
Yuka Noguti <nogutiyuka@gmail.com>
2024-09-04 22:05:02 -0300

    Checkpoint: criação de todas as linhas verdes e vermelhas

--- mobile/lib/pages/carteirinha.dart
+++ mobile/lib/pages/carteirinha.dart
@@ -27,13 +27,19 @@ class CarteirinhaPage extends StatelessWidget {
           final double appBarHeight = AppBar().preferredSize.height;
           final double footerHeight = 40.0;
           final double additionalSpacing = 16.0;
-          final double carouselHeight = screenHeight - appBarHeight - footerHeight - additionalSpacing + 30.0;
+          final double carouselHeight = screenHeight -
+              appBarHeight -
+              footerHeight -
+              additionalSpacing +
+              30.0;
 
           return Column(
             children: [
               Container(
                 height: carouselHeight,
-                child: CarouselWidget(idInformation: idInformation), // Pass ID info to the carousel
+                child: CarouselWidget(
+                    idInformation:
+                        idInformation), // Pass ID info to the carousel
               ),
               SizedBox(height: 15),
               Row(
@@ -79,133 +85,158 @@ class CarouselWidget extends StatelessWidget {
     );
   }
 
-Widget _buildInfoItem(Map<String, String> info) {
-  return Container(
-    color: Colors.white,
-    padding: EdgeInsets.all(16.0),
-    child: Center(
-      child: RotatedBox(
-        quarterTurns: 3, // Rotate 270 degrees (90 degrees counterclockwise)
-        child: Stack(
-          children: [
-            // Top border
-            Positioned(
-              top: 100, // Adjust this to control the distance from the first element
-              left: 15,
-              right: 15,
-              child: Container(
-                height: 1.0, // Height of the border
-                color: Colors.grey, // Border color
+  Widget _buildInfoItem(Map<String, String> info) {
+    return Container(
+      color: Colors.white,
+      padding: EdgeInsets.all(16.0),
+      child: Center(
+        child: RotatedBox(
+          quarterTurns: 3, // Rotate 270 degrees (90 degrees counterclockwise)
+          child: Stack(
+            children: [
+              // Top border
+              Positioned(
+                top:
+                    100, // Adjust this to control the distance from the first element
+                left: 15,
+                right: 15,
+                child: Container(
+                  height: 1.0, // Height of the border
+                  color: Colors.grey, // Border color
+                ),
               ),
-            ),
-            // Left border (red)
-            Positioned(
-              top: 0,
-              bottom: 0,
-              left: 0, // Position the red border on the left
-              child: Container(
-                width: 5.0, // Width of the border
-                color: Colors.green, // Border color
+              // Left border (red)
+              Positioned(
+                top: 0,
+                bottom: 0,
+                left: 0, // Position the red border on the left
+                child: Container(
+                  width: 5.0, // Width of the border
+                  color: Colors.green, // Border color
+                ),
               ),
-            ),
-            // Additional border (e.g., green) next to the red border
-            Positioned(
-              top: 0,
-              bottom: 0,
-              left: 10.0, // Position this border next to the red border
-              child: Container(
-                width: 5.0, // Width of the additional border
-                color: Colors.red, // Color of the additional border
+              // Additional border (e.g., green) next to the red border
+              Positioned(
+                top: 0,
+                bottom: 0,
+                left: 10.0, // Position this border next to the red border
+                child: Container(
+                  width: 5.0, // Width of the additional border
+                  color: Colors.red, // Color of the additional border
+                ),
               ),
-            ),
-            Positioned(
-              top: 0,
-              bottom: 0,
-              right: 0, // Position the red border on the left
-              child: Container(
-                width: 5.0, // Width of the border
-                color: Colors.red, // Border color
+              Positioned(
+                top: 0,
+                bottom: 0,
+                right: 0, // Position the red border on the left
+                child: Container(
+                  width: 5.0, // Width of the border
+                  color: Colors.red, // Border color
+                ),
               ),
-            ),
-            // Additional border (e.g., green) next to the red border
-            Positioned(
-              top: 0,
-              bottom: 0,
-              right: 10.0, // Position this border next to the red border
-              child: Container(
-                width: 5.0, // Width of the additional border
-                color: Colors.green, // Color of the additional border
+              // Additional border (e.g., green) next to the red border
+              Positioned(
+                top: 0,
+                bottom: 0,
+                right: 10.0, // Position this border next to the red border
+                child: Container(
+                  width: 5.0, // Width of the additional border
+                  color: Colors.green, // Color of the additional border
+                ),
               ),
-            ),
-            Positioned(
-              top: -80,
-              bottom: 365,
-              right: 150, 
-              child: Container(
-                width: 5.0, // Width of the border
-                color: Colors.red, // Border color
+              Positioned(
+                top: -80,
+                bottom: 385,
+                right: 150,
+                child: Container(
+                  width: 5.0, // Width of the border
+                  color: Colors.green, // Border color
+                ),
               ),
-            ),
-            Positioned(
-              top: 0,
-              bottom: 0,
-              right: 10.0, // Position this border next to the red border
-              child: Container(
-                width: 5.0, // Width of the additional border
-                color: Colors.green, // Color of the additional border
+              Positioned(
+                top: -80,
+                bottom: 395,
+                right: 140, // Position this border next to the red border
+                child: Container(
+                  width: 5.0, // Width of the additional border
+                  color: Colors.red, // Color of the additional border
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 25.0, top: 30.0), // Adjust padding to avoid overlapping with the borders
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    '${info['nome']}',
-                    style: TextStyle(color: Colors.black, fontSize: 24),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    'Curso: ${info['curso']}',
-                    style: TextStyle(color: Colors.black, fontSize: 18),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    'RA: ${info['ra']}',
-                    style: TextStyle(color: Colors.black, fontSize: 18),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    'Ingresso: ${info['ingresso']}',
-                    style: TextStyle(color: Colors.black, fontSize: 18),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    'Validade: ${info['validade']}',
-                    style: TextStyle(color: Colors.black, fontSize: 18),
-                  ),
-                ],
+
+              //teste
+              Positioned(
+                top:
+                    85, // Adjust this to control the distance from the first element
+                left: 379,
+                right: 15,
+                child: Container(
+                  height: 5.0, // Height of the border
+                  color: Colors.red, // Border color
+                ),
               ),
-            ),
-          ],
+              Positioned(
+                top:
+                    95, // Adjust this to control the distance from the first element
+                left: 368,
+                right: 15,
+                child: Container(
+                  height: 5.0, // Height of the border
+                  color: Colors.green, // Border color
+                ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.only(
+                    left: 25.0,
+                    top:
+                        30.0), // Adjust padding to avoid overlapping with the borders
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      '${info['nome']}',
+                      style: TextStyle(color: Colors.black, fontSize: 24),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Curso: ${info['curso']}',
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'RA: ${info['ra']}',
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Ingresso: ${info['ingresso']}',
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Validade: ${info['validade']}',
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  );
-}
-
-
-
+    );
+  }
 
   Widget _buildQRCodeItem() {
     return Container(
       color: Colors.white,
       child: Center(
         child: Container(
-          width: 500.0,  // Increase the width
+          width: 500.0, // Increase the width
           height: 500.0, // Increase the height
-          child: Icon(Icons.qr_code, size: 250, color: Colors.black), // Make the QR code icon larger
+          child: Icon(Icons.qr_code,
+              size: 250, color: Colors.black), // Make the QR code icon larger
         ),
       ),
     );
