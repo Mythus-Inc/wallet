8481f1d29a0ef228e92c15bd8990a1bcc3a7d5e6
Keren Apuque <cardosokerenapuque@gmail.com>
2024-10-19 21:48:51 -0300

    feat: adicionado método para converter Json para dtoAluno

--- mobile/lib/dto/dto_aluno_login.dart
+++ mobile/lib/dto/dto_aluno_login.dart
@@ -1,10 +1,37 @@
+import 'package:wallet_mobile/models/aluno.dart';
+
 class DtoalunoLogin{
   final String ra;
+  final String? nome;
+  final String? email;
+  final String? senha;
+  final List<String>? alunoTurma;
 
-  DtoalunoLogin({required this.ra});
-  Map<String, dynamic> toJson() {
-    return {
+
+ DtoalunoLogin({
+    required this.ra,
+    this.nome,
+    this.email,
+    this.senha,
+    this.alunoTurma,
+  });
+  
+  Map<String, dynamic> toJson()  {
+    return  {
       'ra': ra,
+      'nome': nome,
+      'email': email,
+      'senha': senha,
+      'alunoTurma': []
     };
+  }
+
+  factory DtoalunoLogin.fromJson(Map<String, dynamic> json) {
+    return DtoalunoLogin(
+      nome: json['nome'],
+      email: json['email'],
+      senha: json['senha'],
+      ra: json['ra'],
+    );
   }
 }
