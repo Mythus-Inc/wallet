f522775e16ed4aef780f369f33793b5548bdc68c
Keren Apuque <cardosokerenapuque@gmail.com>
2024-10-21 01:36:02 -0300

    feat: modificado tipo do parametro para salvar  os dados localmete

--- mobile/lib/service/aluno_service.dart
+++ mobile/lib/service/aluno_service.dart
@@ -1,24 +1,25 @@
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wallet_mobile/dto/dto_aluno_login.dart';
 import 'package:wallet_mobile/models/aluno.dart';
 import 'package:wallet_mobile/values/preferences_key.dart';
 
 class AlunoService{
 
-  static Future<void> salvarAluno(Aluno aluno) async {
+  static Future<void> salvarAluno(DtoalunoLogin aluno) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setString(PreferencesKey.chaveAcessoOuSobreescrita, json.encode(aluno.toJson()));
   }
 
   // Aqui deve ter um método que busca os dados de aluno no Backend (cronos)!!
 
-  static Future<Aluno?> recuperarAlunoSalvo() async {
+  static Future<DtoalunoLogin?> recuperarAlunoSalvo() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? alunoJson = prefs.getString(PreferencesKey.chaveAcessoOuSobreescrita);
     
     if (alunoJson != null) {
       Map<String, dynamic> alunoMap = json.decode(alunoJson);
-      return Aluno.fromJson(alunoMap);
+      return DtoalunoLogin.fromJson(alunoMap);
     }
     return null;
   }
