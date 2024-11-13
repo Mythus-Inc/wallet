996048b8a23e2cb519259579fee764246f7e9685
netorapg <renatoplatz11@outlook.com>
2024-11-13 20:29:55 -0300

    Feat: Trabalhando no design do pdf

--- mobile/lib/pages/carteirinha.dart
+++ mobile/lib/pages/carteirinha.dart
@@ -17,15 +17,44 @@ Future<void> generatePDF(String nome, String curso, String anoEgresse, String va
       pageFormat: PdfPageFormat.a4,
       build: (pw.Context context) {
         return pw.Column(
-          crossAxisAlignment: pw.CrossAxisAlignment.start,
+          crossAxisAlignment: pw.CrossAxisAlignment.stretch,
           children: [
-            pw.Text("Carteirinha do Aluno", style: pw.TextStyle(fontSize: 24, fontWeight: pw.FontWeight.bold)),
+            pw.Container(
+              color: PdfColors.green,
+              height: 50,
+              alignment: pw.Alignment.center,
+              child: pw.Text("Carteirinha do Aluno", style: pw.TextStyle(fontSize: 24, fontWeight: pw.FontWeight.bold, color: PdfColors.white)),
+            ),
+            pw.SizedBox(height: 10),
+            pw.Divider(color: PdfColors.green, thickness: 2),
             pw.SizedBox(height: 20),
-            pw.Text("Nome: $nome", style: pw.TextStyle(fontSize: 18)),
-            pw.Text("Curso: $curso", style: pw.TextStyle(fontSize: 18)),
-            pw.Text("Ano de Egresso: $anoEgresse", style: pw.TextStyle(fontSize: 18)),
-            pw.Text("Validade: $validade", style: pw.TextStyle(fontSize: 18)),
-          ]
+            
+            pw.Table.fromTextArray(
+              border: pw.TableBorder.all(color: PdfColors.grey),
+              cellAlignment: pw.Alignment.centerLeft,
+              headerDecoration: pw.BoxDecoration(color: PdfColors.grey300),
+              headerHeight: 25,
+              cellHeight: 40,
+              headerStyle: pw.TextStyle(fontSize: 18, fontWeight: pw.FontWeight.bold),
+              cellStyle: pw.TextStyle(fontSize: 16),
+              data: [
+                ['Nome:', nome],
+                ['Curso:', curso],
+                ['Ano de Egresso:', anoEgresse],
+                ['Validade:', validade],
+              ],
+            ),
+            pw.SizedBox(height: 20),
+
+            pw.Divider(color: PdfColors.green, thickness: 2),
+            pw.Align(
+              alignment: pw.Alignment.bottomRight,
+              child: pw.Text(
+                "Instituto Federal do Paraná - IFPR",
+                style: pw.TextStyle(fontSize: 12, color: PdfColors.grey),
+              ),
+            ),
+          ],
         );
       },
     ),
